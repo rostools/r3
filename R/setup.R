d5622727dcbdb1f5fa328e4115fb632dc754b578
Luke W. Johnston <lwjohnst@gmail.com>
2020-02-17 22:23:49 +0100

    change name of git setup fun

--- R/setup.R
+++ R/setup.R
@@ -4,12 +4,14 @@
 #' @return Modifies global git configuration.
 #' @export
 #'
-setup_git <- function() {
-    user_name <- rstudioapi::showPrompt("What is your name?",
-                                        "Please type out your first and last name.")
+setup_git_config <- function() {
+    user_name <- rstudioapi::showPrompt(
+        "What is your name?",
+        "Please type out your full name (e.g. first, middle, last)."
+    )
     user_email <- rstudioapi::showPrompt(
         "What is your email?",
-        "Use an email that you'll always have (e.g., your work email is only yours while as an employee)."
+        "Type out your email. It's recommended to use an email like Gmail, as you always have access to it."
     )
     usethis::use_git_config(
         "user",
