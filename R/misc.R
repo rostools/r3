373c3df1ec44d953e0a00f6bf6f8980c546bddd8
Luke W. Johnston <lwjohnst@gmail.com>
2021-05-13 20:24:19 +0200

    fix link

--- R/misc.R
+++ R/misc.R
@@ -2,4 +2,4 @@
 #' Link to MMASH zip data file, just in case.
 #'
 #' @export
-mmash_data_link <- "https://gitlab.com/rostools/r-cubed-intermediate/-/raw/master/data-raw/mmash-data.zip"
+mmash_data_link <- "https://gitlab.com/rostools/r-cubed-intermediate/-/raw/main/data-raw/mmash-data.zip"
