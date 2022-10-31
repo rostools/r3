8ffa5ba7b84336c8d0fba6bc93e997f6bb50a954
Luke W. Johnston <lwjohnst@gmail.com>
2022-10-31 22:47:48 +0100

    function to install packages for advanced coursre

---
+++ R/install-packages.R
@@ -0,0 +1,12 @@
+#' Install the R packages necessary for the Advanced R course.
+#'
+#' @return Outputs nothing, used to install packages for the R course.
+#' @export
+#'
+install_packages_advanced <- function() {
+    description_text <- readLines("https://raw.githubusercontent.com/rostools/r-cubed-advanced/main/DESCRIPTION")
+    # -1 to remove the R dependency from the vector.
+    needed_packages <- desc::desc(text = description_text)$get_deps()$package[-1]
+    pak::pkg_install(needed_packages, ask = FALSE, upgrade = TRUE)
+    return(invisible(NULL))
+}
