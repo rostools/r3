11c58b10698d0a0426d3976d53f7ab869c257bcf
Luke W. Johnston <lwjohnst@gmail.com>
2022-11-22 11:02:56 +0100

    Don't need to install rspm and a few other packages for advanced course

--- R/install-packages.R
+++ R/install-packages.R
@@ -4,9 +4,10 @@
 #' @export
 #'
 install_packages_advanced <- function() {
-    description_text <- readLines("https://raw.githubusercontent.com/rostools/r-cubed-advanced/main/DESCRIPTION")
-    # -1 to remove the R dependency from the vector.
-    needed_packages <- desc::desc(text = description_text)$get_deps()$package[-1]
-    pak::pkg_install(needed_packages, ask = FALSE, upgrade = TRUE)
-    return(invisible(NULL))
+  description_text <- readLines("https://raw.githubusercontent.com/rostools/r-cubed-advanced/main/DESCRIPTION")
+  # -1 to remove the R dependency from the vector.
+  needed_packages <- desc::desc(text = description_text)$get_deps()$package[-1]
+  needed_packages <- stringr::str_subset(needed_packages, "RcppTOML|rspm|todor", negate = TRUE)
+  pak::pkg_install(needed_packages, ask = FALSE, upgrade = TRUE)
+  return(invisible(NULL))
 }
