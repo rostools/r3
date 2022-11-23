74ccf5c79557ad7df806d84fccb1b40dc8314ede
Luke W. Johnston <lwjohnst@gmail.com>
2022-11-23 15:02:31 +0100

    remove r3admin from list of paackages to install

--- R/install-packages.R
+++ R/install-packages.R
@@ -7,7 +7,7 @@ install_packages_advanced <- function() {
   description_text <- readLines("https://raw.githubusercontent.com/rostools/r-cubed-advanced/main/DESCRIPTION")
   # -1 to remove the R dependency from the vector.
   needed_packages <- desc::desc(text = description_text)$get_deps()$package[-1]
-  needed_packages <- stringr::str_subset(needed_packages, "RcppTOML|rspm|todor", negate = TRUE)
+  needed_packages <- stringr::str_subset(needed_packages, "r3admin|RcppTOML|rspm|todor", negate = TRUE)
   pak::pkg_install(needed_packages, ask = FALSE, upgrade = TRUE)
   return(invisible(NULL))
 }
