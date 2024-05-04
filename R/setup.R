565cddbe505592d2b968660cba72195d1cb6213f
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-04 16:32:53 +0200

    feat: function to set rproj settings

--- R/setup.R
+++ R/setup.R
@@ -35,3 +35,49 @@ setup_git_settings <- function() {
     init.defaultBranch = "main"
   )
 }
+
+#' Setup the R project file to simplify learning in the course.
+#'
+#' @return Invisibly outputs the contents of the `.Rproj` file. Writes to file
+#'   as side effect.
+#' @export
+#'
+setup_rproj <- function() {
+  proj <- rstudioapi::getActiveProject()
+  if (is.null(proj)) {
+    cli::cli_abort("You aren't in the R project. Please open the R Project for the course and run this function again in the Console.")
+  }
+
+  rproj_filepath <- fs::dir_ls(proj, regexp = "^.*\\.Rproj")
+  rproj <- readLines(rproj_filepath) |>
+    set_rproj_tab_spaces_to_2() |>
+    set_rproj_native_pipe()
+
+  writeLines(rproj, rproj_filepath)
+  cli::cli_inform("Now using 2 spaces for a tab along with the native R pipe ({.val |>}).")
+  invisible(rproj)
+}
+
+set_rproj_tab_spaces_to_2 <- function(text) {
+  if (sum(stringr::str_detect(text, "UseSpacesForTab|NumSpacesForTab")) == 2) {
+    text <- text |>
+      stringr::str_replace("UseSpacesForTab.*", "UseSpacesForTab: Yes") |>
+      stringr::str_replace("NumSpacesForTab.*", "NumSpacesForTab: 2")
+  } else {
+    text <- text |>
+      append("UseSpacesForTab: Yes") |>
+      append("NumSpacesForTab: 2")
+  }
+  text
+}
+
+set_rproj_native_pipe <- function(text) {
+  if (any(stringr::str_detect(text, "UseNativePipeOperator"))) {
+    text <- text |>
+      stringr::str_replace("UseNativePipeOperator.*", "UseNativePipeOperator: Yes")
+  } else {
+    text <- text |>
+      append("UseNativePipeOperator: Yes")
+  }
+  text
+}
