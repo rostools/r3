5576c81b3f79da8b1752951015c5d4086565405f
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-04 15:51:51 +0200

    feat: add function to fix rebase issue. Closes #9 and #13

--- R/setup.R
+++ R/setup.R
@@ -1,4 +1,3 @@
-
 #' Set the user's git configuration.
 #'
 #' @return Modifies global git configuration.
@@ -6,18 +5,33 @@
 #'
 setup_git_config <- function() {
   # TODO: Run check to make sure not to overwrite if it exists.
-    user_name <- rstudioapi::showPrompt(
-        "What is your name?",
-        "Please type out your full name (e.g. first middle last)."
-    )
-    user_email <- rstudioapi::showPrompt(
-        "What is your email?",
-        "Type out your email. It's recommended to use an email like Gmail, as you always have access to it."
-    )
-    usethis::use_git_config(
-        "user",
-        user.name = user_name,
-        user.email = user_email,
-        init.defaultBranch = "main"
-    )
+  user_name <- rstudioapi::showPrompt(
+    "What is your name?",
+    "Please type out your full name (e.g. first middle last)."
+  )
+  user_email <- rstudioapi::showPrompt(
+    "What is your email?",
+    "Type out your email. It's recommended to use an email like Gmail, as you always have access to it."
+  )
+  usethis::use_git_config(
+    "user",
+    user.name = user_name,
+    user.email = user_email,
+    init.defaultBranch = "main"
+  )
+  setup_git_settings()
+}
+
+# TODO: remove this function after course.
+#' Set some Git config settings like main as default and rebase as false.
+#'
+#' @return Invisible, values of previous settings.
+#' @export
+#'
+setup_git_settings <- function() {
+  usethis::use_git_config(
+    scope = "user",
+    pull.rebase = FALSE,
+    init.defaultBranch = "main"
+  )
 }
