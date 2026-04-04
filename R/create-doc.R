5fdb04ce3ac78daf2f1510aefe950989d6ee6ca3
Luke W. Johnston <lwjohnst@gmail.com>
2026-04-04 18:23:10 +0200

    feat: :sparkles: add a new `create_cleaning_qmd()` function for intermediate

--- R/create-doc.R
+++ R/create-doc.R
@@ -2,17 +2,30 @@
 #'
 #' Make a draft of a Quarto document to use for the workshops.
 #'
-#' @return Creates a Quarto Markdown document in the `docs/` folder.
+#' @name create_qmd
+#' @returns Creates a Quarto Markdown document in the `docs/` folder.
+NULL
+
+#' @describeIn create_qmd Create a `learning.qmd` file in the `docs/` folder.
 #' @export
-#'
-create_qmd_doc <- function() {
+create_learning_qmd <- function() {
+  create_common_qmd("learning.qmd")
+}
+
+#' @describeIn create_qmd Create a `cleaning.qmd` file in the `docs/` folder.
+#' @export
+create_cleaning_qmd <- function() {
+  create_common_qmd("cleaning.qmd")
+}
+
+create_common_qmd <- function(output_file = "basic.qmd") {
   proj <- usethis::proj_path()
   if (is.null(proj)) {
     cli::cli_abort(
       "You aren't in the R Project. Please open the R Project for the workshop and run this function again."
     )
   }
-  new_file_name <- fs::path(proj, "docs", "learning.qmd")
+  new_file_name <- fs::path(proj, "docs", output_file)
   if (!fs::dir_exists(fs::path(proj, "docs"))) {
     fs::dir_create(fs::path(proj, "docs"))
   }
@@ -20,7 +33,7 @@ create_qmd_doc <- function() {
     fs::path_package(
       "r3",
       "templates",
-      "learning.qmd"
+      "common.qmd"
     ),
     new_file_name
   )
